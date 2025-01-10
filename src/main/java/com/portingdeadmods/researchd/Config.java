3da7239389dc04b0f2b039880311b5ad3fd5507c
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2025-01-10 16:48:20 +0200

    Mod naming

---
+++ src/main/java/com/portingdeadmods/researchd/Config.java
@@ -0,0 +1,33 @@
+package com.portingdeadmods.researchd;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+@EventBusSubscriber(modid = Researchd.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class Config
+{
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    private static final ModConfigSpec.BooleanValue DEFAULT_RESEARCH_IMPLEMENTATION = BUILDER
+            .comment("Include the default research implementation")
+            .define("defaultResearchImplementation", true);
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+
+    public static boolean defaultResearchImplementation;
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+        defaultResearchImplementation = DEFAULT_RESEARCH_IMPLEMENTATION.get();
+    }
+}
