c5455d7d054e2296be1a5f740924711daca1812e
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2025-07-19 16:08:42 +0300

    These should be common

--- src/main/java/com/portingdeadmods/researchd/events/ResearchdCommonEvents.java
+++ src/main/java/com/portingdeadmods/researchd/events/ResearchdCommonEvents.java
@@ -1,9 +1,30 @@
 package com.portingdeadmods.researchd.events;
 
 import com.portingdeadmods.researchd.Researchd;
+import com.portingdeadmods.researchd.ResearchdRegistries;
 import com.portingdeadmods.researchd.api.data.PDLClientSavedData;
+import com.portingdeadmods.researchd.api.data.PDLSavedData;
+import com.portingdeadmods.researchd.api.data.SavedDataHolder;
+import com.portingdeadmods.researchd.api.research.Research;
+import com.portingdeadmods.researchd.api.research.ResearchInstance;
 import com.portingdeadmods.researchd.commands.ResearchdCommands;
 import com.portingdeadmods.researchd.data.ResearchdAttachments;
+import com.portingdeadmods.researchd.data.ResearchdSavedData;
+import com.portingdeadmods.researchd.data.helper.ResearchProgress;
+import com.portingdeadmods.researchd.data.helper.ResearchTeam;
+import com.portingdeadmods.researchd.data.helper.ResearchTeamMap;
+import com.portingdeadmods.researchd.impl.research.ResearchPack;
+import com.portingdeadmods.researchd.networking.SyncSavedDataPayload;
+import com.portingdeadmods.researchd.utils.researches.ResearchHelperCommon;
+import com.portingdeadmods.researchd.utils.researches.data.ResearchQueue;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -13,7 +34,11 @@ import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
+import java.util.Map;
 import java.util.UUID;
 
 @EventBusSubscriber(modid = Researchd.MODID)
@@ -38,6 +63,80 @@ public final class ResearchdCommonEvents {
     @SubscribeEvent
     private static void onLeaveWorld(PlayerEvent.PlayerLoggedOutEvent event) {
         PDLClientSavedData.CLIENT_SAVED_DATA_CACHE.clear();
+    }
+
+    @SubscribeEvent
+    public static void onServerTick(ServerTickEvent.Post event) {
+        MinecraftServer server = event.getServer();
+        ServerLevel level = server.overworld();
+
+        ResearchTeamMap data = ResearchdSavedData.TEAM_RESEARCH.get().getData(level);
+
+        if (data != null) {
+            for (Map.Entry<UUID, ResearchTeam> entry : data.getResearchTeams().entrySet()) {
+                ResearchTeam team = entry.getValue();
+                ResearchProgress researchProgress = team.getResearchProgress();
+
+                // v Research Queue Logic v
+                ResearchQueue queue = researchProgress.researchQueue();
+
+                if (!queue.isEmpty()) {
+                    ResearchInstance instance = queue.getEntries().getFirst();
+                    Research research = ResearchHelperCommon.getResearch(instance.getResearch(), server.registryAccess());
+
+                    // TODO: Continue when the ground basis is set n done
+                }
+
+
+                if (level.getGameTime() % 10 == 0) {
+                    ResearchdSavedData.TEAM_RESEARCH.get().setData(level, data);
+                }
+            }
+        }
+    }
+
+    private static <T> void sendSavedDataSyncPayload(ServerPlayer serverPlayer, ResourceLocation id, PDLSavedData<?> savedData) {
+        PDLSavedData<T> savedData1 = (PDLSavedData<T>) savedData;
+        T data = savedData1.getData(serverPlayer.serverLevel());
+        PacketDistributor.sendToPlayer(serverPlayer, new SyncSavedDataPayload<>(new SavedDataHolder<>(id, savedData1), data));
+    }
+
+    @SubscribeEvent
+    private static void onJoinWorld(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity().level().isClientSide()) return;
+        if (!(event.getEntity() instanceof ServerPlayer serverPlayer)) return;
+
+        MinecraftServer server = serverPlayer.server;
+        ServerLevel level = server.overworld();
+        ResearchTeamMap researchTeamMap = ResearchdSavedData.TEAM_RESEARCH.get().getData(level);
+        researchTeamMap.initPlayer(serverPlayer);
+        ResearchdSavedData.TEAM_RESEARCH.get().setData(level, researchTeamMap);
+
+        for (Map.Entry<ResourceKey<PDLSavedData<?>>, PDLSavedData<?>> savedData : ResearchdRegistries.SAVED_DATA.entrySet()) {
+            PDLSavedData<?> value = savedData.getValue();
+            if (value.isSynced()) {
+                sendSavedDataSyncPayload(serverPlayer, savedData.getKey().location(), value);
+                value.onSyncFunction().accept(serverPlayer);
+            }
+        }
+
+        // v Research Predicate Attachment v
+        ResearchHelperCommon.refreshResearches(serverPlayer);
+    }
+
+    @SubscribeEvent
+    public static void onServerAboutToStart(ServerAboutToStartEvent event) {
+        MinecraftServer server = event.getServer();
+        RegistryAccess registryAccess = server.registryAccess();
+        HolderLookup.RegistryLookup<ResearchPack> packs = registryAccess.lookupOrThrow(ResearchdRegistries.RESEARCH_PACK_KEY);
+        Researchd.RESEARCH_PACKS.addAll(packs.listElements().map(Holder.Reference::value).toList());
+        Researchd.debug("Researchd Constants Server", "Initialized research packs.", Researchd.RESEARCH_PACKS, "");
+
+        Researchd.RESEARCH_PACK_COUNT.initialize((int) packs.listElements().count());
+        Researchd.debug("Researchd Constants Server", "Initialized research pack count: ", Researchd.RESEARCH_PACK_COUNT.get());
+
+        Researchd.RESEARCH_PACK_REGISTRY.initialize(registryAccess.lookupOrThrow(ResearchdRegistries.RESEARCH_PACK_KEY));
+        Researchd.debug("Researchd Constants Server", "Initialized research pack registry LazyFinal. ");
     }
 
     public static void onJoinLevel(EntityJoinLevelEvent entity) {
