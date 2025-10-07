957716be9b50e37eb2f567f300557e96032e6aa2
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2025-10-07 22:58:01 +0300

    Refresh tabs correctly on data reload

--- src/main/java/com/portingdeadmods/researchd/networking/research/ResearchCacheReloadPayload.java
+++ src/main/java/com/portingdeadmods/researchd/networking/research/ResearchCacheReloadPayload.java
@@ -1,6 +1,5 @@
 package com.portingdeadmods.researchd.networking.research;
 
-import com.portingdeadmods.portingdeadlibs.PortingDeadLibsClient;
 import com.portingdeadmods.researchd.Researchd;
 import com.portingdeadmods.researchd.cache.CommonResearchCache;
 import com.portingdeadmods.researchd.client.cache.ResearchGraphCache;
@@ -9,9 +8,11 @@ import com.portingdeadmods.researchd.data.ResearchdSavedData;
 import com.portingdeadmods.researchd.impl.team.ResearchTeamMap;
 import com.portingdeadmods.researchd.impl.team.SimpleResearchTeam;
 import com.portingdeadmods.researchd.utils.researches.ResearchHelperClient;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jetbrains.annotations.NotNull;
@@ -41,7 +42,11 @@ public record ResearchCacheReloadPayload() implements CustomPacketPayload {
             ResearchGraphCache.clearCache();
             ClientResearchTeamHelper.refreshResearchScreenData();
 
-            PortingDeadLibsClient.markTabsDirty();
+            var mc = Minecraft.getInstance();
+            if (mc.player != null) {
+                CreativeModeTabs.CACHED_PARAMETERS = null;
+                CreativeModeTabs.tryRebuildTabContents(mc.player.connection.enabledFeatures(), mc.player.canUseGameMasterBlocks() && mc.options.operatorItemsTab().get(), mc.level.registryAccess());
+            }
         }).exceptionally(err -> {
            Researchd.LOGGER.error("Encountered error while handling ResearchCacheReloadPayload", err);
            return null;
